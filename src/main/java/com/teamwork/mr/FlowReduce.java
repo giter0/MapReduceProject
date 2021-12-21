2b4b5e9f66f97613ecaa1e1d3a607bcdbe516c1c
clxzy <549547285@qq.com>
2021-12-22 03:08:39 +0800

    Update FlowReduce.java

--- src/main/java/com/teamwork/mr/FlowReduce.java
+++ src/main/java/com/teamwork/mr/FlowReduce.java
@@ -16,6 +16,7 @@ public class FlowReduce extends Reducer<FlowBean, Text,Text,FlowBean> {
     // 数据整合
     @Override
     protected void reduce(FlowBean key, Iterable<Text> values, Reducer<FlowBean, Text, Text, FlowBean>.Context context) throws IOException, InterruptedException {
+
         //遍历 values 集合,循环写出,避免总流量相同的情况
         for (Text value : values) {
             //调换 KV 位置,反向写出
