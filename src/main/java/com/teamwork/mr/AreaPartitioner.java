b979a9510d8f6e1f4abbfc37240cb7e96954c316
jianzihuai <90249239+jianzihuai@users.noreply.github.com>
2021-12-21 14:48:10 +0800

    分区及Driver
    完成AreaPartitioner及FlowDriver的设计，需要补充的或者更改已备注好

--- src/main/java/com/teamwork/mr/AreaPartitioner.java
+++ src/main/java/com/teamwork/mr/AreaPartitioner.java
@@ -1,13 +1,51 @@
 package com.teamwork.mr;
 
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
 /**
- * @author
+ * @author Jane
  * @version 1.0
- * @description: TODO
+ * @description: TODO 自定义分区类，分区按照运营商设置。
  * @date 2021/12/21
  */
-public class AreaPartitioner {
+public class AreaPartitioner extends Partitioner<FlowBean ,Text>{
 
     // 设置分区
 
+    @Override
+    public int getPartition(FlowBean flowBean, Text text, int numPartitions) {
+        // text 是手机号
+
+        //
+        /**
+         * 设置五个分区(按照运营商来设置，四个区代表四个不同的运营商)
+         * area1 area2 area3 area4
+         * 第五分区为不属于这四个运营商的数据
+         */
+        String area1 = 001;
+        String area2 = 002;
+        String area3 = 003;
+        String area4 = 004;
+
+        String phone = text.toString();
+
+        // areaIndex 为需要对那一列属性分区
+        int areaIndex ;
+        String prePhone = phone.substring(0, areaIndex);
+
+        int partition;
+        if (area1.equals(prePhone)) {
+            partition = 0;
+        } else if (area2.equals(prePhone)) {
+            partition = 1;
+        } else if (area3.equals(prePhone)) {
+            partition = 2;
+        } else if (area4.equals(prePhone)) {
+            partition = 3;
+        } else {
+            partition = 4;
+        }
+        return partition;
+    }
 }
--- src/main/java/com/teamwork/mr/FlowDriver.java
+++ src/main/java/com/teamwork/mr/FlowDriver.java
@@ -1,13 +1,67 @@
 package com.teamwork.mr;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 /**
- * @author
+ * @author Jane
  * @version 1.0
- * @description: TODO
+ * @description: TODO 需要设置集群文件输入输出路径
  * @date 2021/12/21
  */
 public class FlowDriver {
 
     //实现job对象
 
+    /**
+     * 要求每个省份手机号输出的文件中按照总流量内部排序。
+     *
+     */
+    public static void main(String[] args) throws IOException,
+            ClassNotFoundException, InterruptedException {
+
+        /**
+         * 设置集群测试文件输入 inputPath
+         * 设置运算结果输出路径 outputPath
+         */
+        String inputPath="";
+        String outputPath="";
+
+        //1 获取 job 对象
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+        //2 关联本 Driver 类
+        job.setJarByClass(FlowDriver.class);
+        //3 关联 Mapper 和 Reducer
+        job.setMapperClass(FlowMapper.class);
+        job.setReducerClass(FlowReducer.class);
+
+//4 设置 Map 端输出 KV 类型
+        job.setMapOutputKeyClass(FlowBean.class);
+        job.setMapOutputValueClass(Text.class);
+
+//5 设置程序最终输出的 KV 类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(FlowBean.class);
+
+        /**
+         * 如果修改了运营商的数量（默认有四个不同的运营商），则需要更改ReduceTasks分区数 areaReduceTasks
+         */
+        int areaReduceTasks = 5;
+        job.setPartitionerClass(AreaPartitioner.class);
+        job.setNumReduceTasks(areaReduceTasks5);
+
+//6 设置程序的输入输出路径
+        args = new String[]{inputPath,outputPath};
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+//7 提交 Job
+        boolean b = job.waitForCompletion(true);
+        System.exit(b ? 0 : 1);
+    }
 }
