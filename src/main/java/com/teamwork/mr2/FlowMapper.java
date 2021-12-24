661236b01f8721d215d57d700083990fd56ff278
miyaaaa1 <90254076+miyaaaa1@users.noreply.github.com>
2021-12-24 09:04:15 +0800

    Update FlowMapper.java

--- src/main/java/com/teamwork/mr2/FlowMapper.java
+++ src/main/java/com/teamwork/mr2/FlowMapper.java
@@ -1,39 +1,47 @@
 package com.teamwork.mr2;
-
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper;
+
 import java.io.IOException;
 
+//Flowbean作为key，Text是手机号
+public class FlowMapper extends Mapper<LongWritable,Text, FlowBean,Text> {
 
-//进入到reducer中都是总流量相同的数据
-public class FlowReducer extends Reducer<FlowBean,Text, Text,FlowBean> {
+    private FlowBean outK=new FlowBean();
+    private Text outV=new Text();
 
-    private FlowBean outV=new FlowBean();
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        //获取一行
+        String line=value.toString();
 
-    protected void reduce(Iterable<FlowBean> key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        //2.切割" "一个空格（文档上的是tab键/t）
+        String[] split = line.split("\t");
 
-        //遍历集合累加值
-        long totalup = 0;
-        long totaldown = 0;
-//        ArrayList<FlowBean> tmpBean = new ArrayList<>();
-//        tmpBean.add(key);
-        for (FlowBean a : key) {
-            totalup += a.getUpFlow();
-            totaldown += a.getDownFlow();
-            outV.setId(a.getId());
-            outV.setPhone(a.getPhone());
-            //outK.set(a.getPhone());
-            System.err.println(a.getPhone());
+        //封装(mapper中：key是流量；v:手机号,)
+        //3.抓取我想要的数据（手机号，上行流量，下行流量，总流量)
+        String phone = split[0];
+        String up = "0";
+        String down = "0";
+        String id = " ";
+        if (!split[split.length - 4].equals("")) {
+            up = split[split.length - 4];
         }
+        if (!split[split.length - 3].equals("")) {
+            down = split[split.length - 3];
+        }
+        if (!split[split.length - 2].equals("")) {
+            id = split[split.length - 2];
+        }
+        outV.set(phone);
+        outK.setOperator(split[split.length - 1]);
+        outK.setUpFlow(Long.parseLong(up));
+        outK.setDownFlow(Long.parseLong(down));
+        outK.setSumFlow();
+        outK.setId(id);
 
-        //2.封装
-        outV.setUpFlow(totalup);
-        outV.setDownFlow(totaldown);
-        outV.setSumFlow();
-
-        //3.写出
-        for (Text value : values) {
-        context.write(value,outV);}
-
+        //写出
+        context.write(outK,outV);
     }
 }
